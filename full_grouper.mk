53ec51958a379e986d293723a4b9ec2088ce60ce
Akihiro Maeda <sola.1980.a@gmail.com>
2014-03-30 01:28:46 +0900

    Set to build aosp and full
    full: aosp + gapps
    aosp: only aosp

--- full_grouper.mk
+++ full_grouper.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2010 The Android Open Source Project
+#
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,12 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#
-# This file is the build configuration for a full Android
-# build for grouper hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps).
 #
 
 # Inherit from those products. Most specific first.
@@ -25,8 +20,15 @@ $(call inherit-product, device/asus/grouper/device.mk)
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.4.2 KOT49H 937116 release-keys"
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_grouper
 PRODUCT_DEVICE := grouper
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Grouper
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+#AOSP
+$(call inherit-product-if-exists, vendor/aosp/asus/grouper/full.mk)
+$(call inherit-product, device/aosp/common/full.mk)
